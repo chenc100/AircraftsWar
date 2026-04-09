646e25c1381cfd7baab4858256f27441b4895864
chenc100 <1773605936@qq.com>
2026-04-09 21:49:46 +0800

    更新了uml类图

--- src/edu/hitsz/aircraft/BossEnemy.java
+++ src/edu/hitsz/aircraft/BossEnemy.java
@@ -1,10 +1,11 @@
 package edu.hitsz.aircraft;
 
+import edu.hitsz.bulletStrategy.Circle;
 import edu.hitsz.bulletStrategy.Direct;
 
 public class BossEnemy extends AbstractEnemy {
 
-    private int shootNum = 20;
+    private int shootNum = 10;
     @Override
     public double getDropProb(){
         return 1;
@@ -17,8 +18,7 @@ public class BossEnemy extends AbstractEnemy {
 
     public BossEnemy(int locationX, int locationY, int speedX, int speedY, int hp){
         super(locationX, locationY, speedX, speedY, hp);
-        bulletStrategy = new Direct(shootNum);
+        bulletStrategy = new Circle(shootNum);
     }
-
 
 }
--- src/edu/hitsz/bulletStrategy/Circle.java
+++ src/edu/hitsz/bulletStrategy/Circle.java
@@ -1,18 +1,56 @@
 package edu.hitsz.bulletStrategy;
 
 import edu.hitsz.aircraft.AbstractAircraft;
+import edu.hitsz.aircraft.AbstractEnemy;
+import edu.hitsz.aircraft.HeroAircraft;
 import edu.hitsz.bullet.BaseBullet;
+import edu.hitsz.bullet.EnemyBullet;
+import edu.hitsz.bullet.HeroBullet;
 
 import java.util.Base64;
+import java.util.LinkedList;
 import java.util.List;
-/*
+
 public class Circle implements BulletStrategy{
+    private int shootNum;
+
+    public Circle(int shootNum){
+        this.shootNum = shootNum;
+    }
+
     @Override
-    public List<BaseBullet> setShoot(AbstractAircraft aircraft){
-        List<BaseBullet> bullets;
+    public List<BaseBullet> setShoot(AbstractAircraft aircraft) {
+        List<BaseBullet> bullets = new LinkedList<>();
 
+        int speed = 3;
+        int radius;
 
+        if (aircraft instanceof HeroAircraft) {
+            radius = 45;
+        } else {
+            radius = 110;
+        }
+
+        for (int i = 0; i < shootNum; i++) {
+
+            double angle = i * (360.0 / shootNum);
+            double rad = Math.toRadians(angle);
+
+            int bulletX = aircraft.getLocationX() + (int) (radius * Math.cos(rad));
+            int bulletY = aircraft.getLocationY() - (int) (radius * Math.sin(rad));
+
+            int dx = (int) (speed * Math.cos(rad));
+            int dy = -(int) (speed * Math.sin(rad));
+
+            BaseBullet bullet;
+            if (aircraft instanceof AbstractEnemy) {
+                bullet = new EnemyBullet(bulletX, bulletY, dx, dy, power);
+            } else {
+                bullet = new HeroBullet(bulletX, bulletY, dx, dy, power);
+            }
+
+            bullets.add(bullet);
+        }
         return bullets;
     }
 }
-*/
